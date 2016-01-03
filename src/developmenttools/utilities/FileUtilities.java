d53a7b6db5db1b1a2c22ea6da37914a31e8b6e65
Efthymiou Dimitrios <efthymiou.dimitrios1@gmail.com>
2016-01-03 02:11:47 +0000

    created some file utilities

---
+++ src/developmenttools/utilities/FileUtilities.java
@@ -0,0 +1,118 @@
+package developmenttools.utilities;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import developmenttools.cloningscript.AddProjectToCloningScript;
+
+public class FileUtilities
+{
+    public InputStream loadFileStream(Class<?> aClass, String fileLocation)
+    {
+        
+        ClassLoader classLoader = null;
+        
+        try
+        {
+            classLoader = Class.forName(aClass.getName()).getClassLoader();
+        }
+        catch(ClassNotFoundException exception)
+        {
+            exception.printStackTrace();
+        }
+        
+        return classLoader.getResourceAsStream(fileLocation);
+    }
+    
+    
+    public String convertFileToString(String filePath)
+    {
+        String fileString = "";
+        BufferedReader input = null;
+        
+        try
+        {
+            input = new BufferedReader(new FileReader(filePath));
+            StringBuilder fileStringBuilder = new StringBuilder();
+            String currentLine = input.readLine();
+
+            while(currentLine != null)
+            {
+                fileStringBuilder.append(currentLine);
+                fileStringBuilder.append(System.lineSeparator());
+                currentLine = input.readLine();
+            }
+            
+            fileString = fileStringBuilder.toString();
+        }
+        catch(FileNotFoundException e)
+        {
+            
+        }
+        catch(IOException e)
+        {
+            
+        }
+        finally
+        {
+            if(input != null)
+            {
+                try
+                {
+                    input.close();
+                }
+                catch(IOException e)
+                {
+                    
+                }
+            }
+        }
+
+        return fileString;
+    }
+
+
+    public void saveStringToFile(String filePath, String fileString)
+    {
+        BufferedWriter output = null;
+        String lineSeparator = System.lineSeparator();
+        
+        try
+        {
+            output = new BufferedWriter(new FileWriter(filePath));
+            String[] lines = fileString.split(lineSeparator);
+            
+            for(String line : lines)
+            {
+                output.write(line);
+                output.write(lineSeparator);
+            }
+        }
+        catch(FileNotFoundException e)
+        {
+            
+        }
+        catch(IOException e)
+        {
+            
+        }
+        finally
+        {
+            if(output != null)
+            {
+                try
+                {
+                    output.close();
+                }
+                catch(IOException e)
+                {
+                    
+                }
+            }
+        }
+    }
+}
